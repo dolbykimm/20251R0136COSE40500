11baf8a77471f9b2ba7c4f2870ddca39b6c3defa
dolbykimm <dolbykimm@gmail.com>
2025-06-16 00:12:20 +0900

    2. 날짜 출력 예제

---
+++ 0627.py
@@ -0,0 +1,22 @@
+import pyautogui
+import time
+
+for i in (1,100):
+    pyautogui.moveTo(693,294)
+    pyautogui.click()
+
+    pyautogui.moveTo(947,292)
+    pyautogui.click()
+
+    pyautogui.moveTo(1110,638)
+    pyautogui.click()
+
+    time.sleep(1) 
+
+    pyautogui.moveTo(821,292)
+    pyautogui.click()
+
+    pyautogui.moveTo(1110,638)
+    pyautogui.click()   
+
+    time.sleep(10)
